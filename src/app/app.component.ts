1a5aa756505056165147de99ca22ecc99cf3c475
Dmitrii Holubtsov <jamie.cadenza@gmail.com>
2021-10-05 15:43:07 +0300

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-task';
+  startDate = new Date(2020, 0, 2);
+
+  constructor(private httpService: HttpClient) {
+  }
+
+  arrDataJson: string [];
+
+  ngOnInit() {
+    this.httpService.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json').subscribe(
+      data => {
+        this.arrDataJson = data as string [];
+         // var name = data.find(r030);
+         console.log(name);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
 }
+// "r030":840,"txt":"Долар США","rate":27.0566,"cc":"USD","exchangedate":"18.03.2020"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { AngularMaterialModule } from './angular-material.module';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
+    HttpClientModule,
+    MatSortModule,
     BrowserModule,
-    AppRoutingModule
+    MatTableModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatPaginatorModule,
+    AngularMaterialModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
